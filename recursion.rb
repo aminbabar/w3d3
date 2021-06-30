3d1c8aa41132edf9896f4d2bc18726c560252e66
aminbabar <amin.babar1@gmail.com>
2021-06-30 15:27:45 -0400

    code

--- recursion.rb
+++ recursion.rb
@@ -1,2 +1,15 @@
 
 
+def iterative_range(start, finish)
+    new_arr = []
+    (start...finish).each { |i| new_arr << i }
+    new_arr
+end
+# p iterative_range(1, -3)
+# p iterative_range(0, 10)
+
+
+def rec_range(start, end)
+
+end
+
